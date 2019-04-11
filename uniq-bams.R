b9db28035c385d88b10110e7a2dabec9cef6722a
shansabri <shanasabri@gmail.com>
2019-04-10 17:56:30 -0700

    upd and write out MSA

--- uniq-bams.R
+++ uniq-bams.R
@@ -2,7 +2,7 @@
 rm(list = ls(all = TRUE))
 options(warn = -1)
 setwd("~/Dropbox/PlathLab/Analyses/Tom_Allison/2019-03-20/")
-pacman::p_load(GenomicAlignments)
+pacman::p_load(GenomicAlignments, msa)
 
 
 
@@ -39,3 +39,26 @@ sapply(samples, function(x){
   message(sprintf("%s\n\tShared between A and BC: %s\n\tShared between B and AC: %s\n\tShared between C and AB: %s", 
           x, shared_chrA_w_chrsBC, shared_chrB_w_chrsAC, shared_chrC_w_chrsAB))
 })
+
+
+## MSA TO FIND OVERLAPPING REGIONS
+fq <- file.path(getwd(), "new_chrs.fa")
+seq <- readDNAStringSet(fq)
+alignment <- msa(seq, "ClustalOmega", verbose = TRUE)
+print(alignment, showNames=FALSE, show="complete")
+tex_file <- file.path(getwd(), "alignment.tex")
+# msaPrettyPrint(alignment, output="tex", showNames="none", showLogo="none", askForOverwrite=FALSE, file = tex_file)
+# tools::texi2pdf(tex_file, clean=TRUE)
+
+msaPrettyPrint(alignment, output="pdf", showNames="none", showLogo="none", askForOverwrite=FALSE, file = gsub("tex", "pdf", tex_file))
+
+chunkSize <- 2500
+for (start in seq(1, ncol(alignment), by=chunkSize)) {
+  end <- min(start + chunkSize - 1, ncol(alignment))
+  alnPart <- DNAMultipleAlignment(subseq(unmasked(alignment), start, end))
+  msaPrettyPrint(x=alnPart, output="pdf", subset=NULL, file=file.path(getwd(), "msa", paste0("aln_", start, "-", end, ".pdf")))
+}
+
+
+
+
